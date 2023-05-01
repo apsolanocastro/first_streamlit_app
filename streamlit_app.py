c2b9db3c33eb00b98a253d170c978a824cd8b074
apsolanocastro <132233997+apsolanocastro@users.noreply.github.com>
2023-05-01 09:23:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast')
+streamlit.text('🥑🍞Aguacate Toast')
                  
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
